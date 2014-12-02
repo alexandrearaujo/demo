d88eb4258bba1a96382b0b4e9046b807581ecd1c
Maurício Roberto <mauricio.robjunior@gmail.com>
2014-12-02 17:35:12 -0300

    alteração das consultas.

--- src/main/java/br/com/mv/dispensacaomedicamento/repository/FarmaciaRepository.java
+++ src/main/java/br/com/mv/dispensacaomedicamento/repository/FarmaciaRepository.java
@@ -10,9 +10,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import br.com.mv.dispensacaomedicamento.model.Estabelecimento;
 import br.com.mv.dispensacaomedicamento.model.Farmacia;
 import br.com.mv.dispensacaomedicamento.model.FarmaciaEstabelecimento;
+import br.com.mv.dispensacaomedicamento.model.VinculoProfissionalDTO;
 
 /**
  * @author joao.franco
@@ -22,7 +22,7 @@ import br.com.mv.dispensacaomedicamento.model.FarmaciaEstabelecimento;
 public interface FarmaciaRepository extends CrudRepository<Farmacia, Long>
 {
 
-		@Query("select "+
+		@Query(value="select "+
 					"fm.cd_farmacia_estabelecimento,"+
 					"f.nr_crf, "+
 					"f.ds_farmacia, "+
@@ -37,37 +37,23 @@ public interface FarmaciaRepository extends CrudRepository<Farmacia, Long>
                 "	 (f.nr_crf = :farmaciaEstabelecimento.crf or :farmaciaEstabelecimento.crf is null) "+
                 "and (f.ds_farmacia like :farmaciaEstabelecimento.getFarmacia().getDescricaoFarmacia()% or :farmaciaEstabelecimento.getFarmacia().getDescricaoFarmacia() is null) "+
                 "and (se.ds_setor_estabelecimento like :farmaciaEstabelecimento.getEstabelecimento().getNomeFantasia()% or :farmaciaEstabelecimento.getEstabelecimento().getNomeFantasia() is null) "+
-                "and (e.nm_fantasia like :farmaciaEstabelecimento.getFarmacia().getEstabelecimentoSetorEstabelecimento()% or :farmaciaEstabelecimento.getFarmacia().getEstabelecimentoSetorEstabelecimento() is null) ")
+                "and (e.nm_fantasia like :farmaciaEstabelecimento.getFarmacia().getEstabelecimentoSetorEstabelecimento()% or :farmaciaEstabelecimento.getFarmacia().getEstabelecimentoSetorEstabelecimento() is null) ", nativeQuery = true)
 	    public Collection<Farmacia> buscarFarmacia(@Param("farmaciaEstabelecimento") FarmaciaEstabelecimento farmaciaEstabelecimento);
 
-	    public Collection<VinculoProfissionalDTO> listarFarmaceutico(String descricao, Estabelecimento estabelecimento)
-	    {
-	        StringBuffer sql = new StringBuffer(+
-	        
-	        "select "+
-	        "distinct(vp.cd_estabelecimento) AS \"idEstabelecimento\", "+
-	        "p.cd_cidadao AS \"idCidadao\", "+
-	        "p.cd_profissional AS \"idProfissional\", "+
-	        "cid.nm_cidadao AS \"nomeCidadao\" "+
-	        "from dbamvfor.tb_profissional p "+
-	        "inner join dbamvfor.tb_vinculo_profissional vp on p.cd_profissional = vp.cd_profissional "+
-	        "inner join dbamvfor.mv_ocupacao_cbo ocbo on ocbo.id_ocupacao = vp.cd_ocupacao "+
-	        "inner join DBAMVFOR.tb_cidadao cid on cid.cd_cidadao = p.cd_cidadao "+
-	        "where (ocbo.descr like '%FARMACE%' OR ocbo.descr like '%FARMACÊ%') "+
-	        "and cid.nm_cidadao like :nomeCidadao "+
-	        "and vp.cd_estabelecimento = :idEstabelecimento "+
-	        
-	        
-	        SQLQuery query = getSession().createSQLQuery(sql.toString()+
-	        query.setResultTransformer(new AliasToBeanResultTransformer(VinculoProfissionalDTO.class)+
-	        
-	        query.setParameter("nomeCidadao", descricao.concat("%")+
-	        query.setParameter("idEstabelecimento", estabelecimento.getId()+
-	        
-	        Collection<VinculoProfissionalDTO> listarFarmaceutico = query.list(+
-	        
-	        return listarFarmaceutico;
-	    }
+		@Query("select "+
+		        "distinct " + 
+		        "vp.cd_estabelecimento, "+
+		        "p.cd_cidadao, "+
+		        "p.cd_profissional, "+
+		        "cid.nm_cidadao "+
+		        "from dbamvfor.tb_profissional p "+
+		        "inner join dbamvfor.tb_vinculo_profissional vp on p.cd_profissional = vp.cd_profissional "+
+		        "inner join dbamvfor.mv_ocupacao_cbo ocbo on ocbo.id_ocupacao = vp.cd_ocupacao "+
+		        "inner join DBAMVFOR.tb_cidadao cid on cid.cd_cidadao = p.cd_cidadao "+
+		        "where (ocbo.descr like '%FARMACE%' OR ocbo.descr like '%FARMACÊ%') "+
+		        "and cid.nm_cidadao like :nomeCidadao% "+
+		        "and vp.cd_estabelecimento = :idEstabelecimento ")
+		public Collection<VinculoProfissionalDTO> listarFarmaceutico(@Param("nomeCidadao") String descricao, @Param("idEstabelecimento") Long idEstabelecimento);
 
 
 }
--- src/main/java/br/com/mv/dispensacaomedicamento/repository/MarcaMedicamentoRepository.java
+++ src/main/java/br/com/mv/dispensacaomedicamento/repository/MarcaMedicamentoRepository.java
@@ -1,41 +1,25 @@
 package br.com.mv.dispensacaomedicamento.repository;
 
-import java.math.BigDecimal;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-import org.hibernate.SQLQuery;
-import org.springframework.dao.DataIntegrityViolationException;
+import br.com.mv.dispensacaomedicamento.model.MarcaMedicamento;
 
-import br.com.mv.commons.web.dao.GenericDao;
-import br.com.mv.commons.web.exception.NotUniqueIdException;
-import br.com.mv.regulacao.dispensacaomedicamento.exception.DispensacaoMedicamentoException;
-import br.com.mv.regulacao.dispensacaomedicamento.model.MarcaMedicamento;
 
-/**
- * @author Francisco Vernek
- *
- */
-
-public interface MarcaMedicamentoRepository extends GenericDao<MarcaMedicamento>
+@Repository
+public interface MarcaMedicamentoRepository extends CrudRepository<MarcaMedicamento, Long>
 {
 
-    @Override
-    public boolean existeMarcaMedicamentoCadastrada(MarcaMedicamento marcaMedicamento) throws DispensacaoMedicamentoException,
-            DataIntegrityViolationException, NotUniqueIdException, InstantiationException, IllegalAccessException, ClassNotFoundException
+	@Query("select count(mm.cd_marca_medicamento) from marca_medicamento mm where mm.ds_marca_medicamento = :descricao ")
+	public Long countMarcaMedicamentoPorDescricao(@Param("descricao") String descricao);
+	
+	default public boolean existeMarcaMedicamentoCadastrada(String descricao)
     {
-        StringBuilder sql = new StringBuilder();
-        
-        sql.append("select ");
-        sql.append("count(mm.cd_marca_medicamento) ");
-        sql.append("from marca_medicamento mm ");
-        sql.append("where mm.ds_marca_medicamento = :descricao ");
+		Long quantidade = countMarcaMedicamentoPorDescricao(descricao);
 
-        
-        SQLQuery query = getSession().createSQLQuery(sql.toString());
-        query.setParameter("descricao", marcaMedicamento.getDescricao());
-        
-        BigDecimal count = (BigDecimal) query.uniqueResult();
-        
-        if(count.intValue() == 0 )
+        if(quantidade.intValue() == 0 )
             return false;
         else
             return true;
