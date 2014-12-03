79f4028c1f8115e551d785acd095a9067bbc1e6a
Rafael de Castro Cunha <rafael.cunha@mv.com.br>
2014-12-03 16:37:35 -0300

    Correção de erros do business

--- src/main/java/br/com/mv/dispensacaomedicamento/business/TipoRelatorioBusiness.java
+++ src/main/java/br/com/mv/dispensacaomedicamento/business/TipoRelatorioBusiness.java
@@ -1,44 +1,17 @@
 package br.com.mv.dispensacaomedicamento.business;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.inject.Inject;
-import javax.inject.Named;
+import br.com.mv.dispensacaomedicamento.repository.TipoRelatorioRepository;
 
-import org.hibernate.criterion.Order;
-
-import br.com.mv.commons.web.business.GenericManagerImpl;
-import br.com.mv.commons.web.util.hibernate.transform.ProjectionFilter;
-import br.com.mv.regulacao.dispensacaomedicamento.dao.TipoRelatorioDao;
-import br.com.mv.regulacao.dispensacaomedicamento.model.TipoRelatorio;
-
-
-@Named("tipoRelatorioManager")
-public class TipoRelatorioBusiness extends GenericManagerImpl<TipoRelatorio, TipoRelatorioRepository> implements TipoRelatorioManager
+@Service
+@Transactional
+public class TipoRelatorioBusiness
 {
 
-    
-    @Inject
-    @Override
-    public void setDao(TipoRelatorioRepository dao)
-    {
-        super.setDao(dao);
-    }
+	@Autowired
+    private TipoRelatorioRepository tipoRelatorioRepository;
 
-    @Override
-    public Collection<TipoRelatorio> listarTipoRelatorio(String nomeTipoRelatorio)
-    {
-        ArrayList<ProjectionFilter> arrayList = new ArrayList<ProjectionFilter>();
-        arrayList.add(new ProjectionFilter("id"));
-        arrayList.add(new ProjectionFilter("nomeTipoRelatorio", nomeTipoRelatorio));
-        arrayList.add(new ProjectionFilter("descricaoTipoRelatorio"));
-        arrayList.add(new ProjectionFilter(Order.asc("nomeTipoRelatorio")));
-        
-        ProjectionFilter [] projectionFilters = new ProjectionFilter[arrayList.size()];
-        return get(0, arrayList.toArray(projectionFilters));
-    }
-
-    
-    
 }
--- src/main/java/br/com/mv/dispensacaomedicamento/repository/TipoRelatorioRepository.java
+++ src/main/java/br/com/mv/dispensacaomedicamento/repository/TipoRelatorioRepository.java
@@ -1,5 +1,7 @@
 package br.com.mv.dispensacaomedicamento.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.mv.dispensacaomedicamento.model.TipoRelatorio;
@@ -7,4 +9,16 @@ import br.com.mv.dispensacaomedicamento.model.TipoRelatorio;
 public interface TipoRelatorioRepository extends CrudRepository<TipoRelatorio,Long>
 {
 
+//    public Collection<TipoRelatorio> listarTipoRelatorio(String nomeTipoRelatorio)
+//    {
+//        ArrayList<ProjectionFilter> arrayList = new ArrayList<ProjectionFilter>();
+//        arrayList.add(new ProjectionFilter("id"));
+//        arrayList.add(new ProjectionFilter("nomeTipoRelatorio", nomeTipoRelatorio));
+//        arrayList.add(new ProjectionFilter("descricaoTipoRelatorio"));
+//        arrayList.add(new ProjectionFilter(Order.asc("nomeTipoRelatorio")));
+//        
+//        ProjectionFilter [] projectionFilters = new ProjectionFilter[arrayList.size()];
+//        return get(0, arrayList.toArray(projectionFilters));
+//    }
+    public Collection<TipoRelatorio> listarTipoRelatorio(String nomeTipoRelatorio);
 }
