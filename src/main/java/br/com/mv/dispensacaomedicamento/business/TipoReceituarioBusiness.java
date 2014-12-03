8fa381875a0f2517f23e727468ab639ab4f7b62b
Rafael de Castro Cunha <rafael.cunha@mv.com.br>
2014-12-03 16:53:44 -0300

    Retirando erros do business

--- src/main/java/br/com/mv/dispensacaomedicamento/business/TipoReceituarioBusiness.java
+++ src/main/java/br/com/mv/dispensacaomedicamento/business/TipoReceituarioBusiness.java
@@ -1,121 +1,75 @@
 package br.com.mv.dispensacaomedicamento.business;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
-import javax.inject.Inject;
-import javax.inject.Named;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import org.hibernate.criterion.Order;
+import br.com.mv.dispensacaomedicamento.model.TipoReceituario;
+import br.com.mv.dispensacaomedicamento.repository.TipoReceituarioRepository;
 
-import br.com.mv.commons.web.business.GenericManagerImpl;
-import br.com.mv.commons.web.exception.NotUniqueIdException;
-import br.com.mv.commons.web.util.hibernate.transform.ProjectionFilter;
-import br.com.mv.regulacao.dispensacaomedicamento.dao.TipoReceituarioDao;
-import br.com.mv.regulacao.dispensacaomedicamento.model.TipoReceituario;
-
-@Named("tipoReceituarioManager")
-public class TipoReceituarioBusiness extends GenericManagerImpl<TipoReceituario, TipoReceituarioRepository> implements TipoReceituarioManager
+@Service
+@Transactional
+public class TipoReceituarioBusiness
 {
 
-    @Inject
-    @Override
-    public void setDao(TipoReceituarioRepository dao)
-    {
-        super.setDao(dao);
-    }
-
-    @Override
-    public Long count(TipoReceituario tipoReceituario)
-    {
-        return count(criarProjection(tipoReceituario));
-    }
-    
-    @Override
+	@Autowired
+    private TipoReceituarioRepository tipoReceituarioRepository;
+	
     public Collection<TipoReceituario> listarPaginado(int initialPos, int maxResults, TipoReceituario tipoReceituario)
     {
-        return fetch(initialPos, maxResults, criarProjection(tipoReceituario));
+//        return fetch(initialPos, maxResults, criarProjection(tipoReceituario));
+    	
+//    	private ProjectionFilter[] criarProjection(TipoReceituario tipoReceituario)
+//        {
+//
+//            ArrayList<ProjectionFilter> listaProjection = new ArrayList<ProjectionFilter>();
+//            
+//            if(tipoReceituario.getId() != null)
+//                listaProjection.add(new ProjectionFilter("id", tipoReceituario.getId()));
+//            
+//            listaProjection.add(new ProjectionFilter("id"));
+//            if(tipoReceituario.getDescricao() != null && !tipoReceituario.getDescricao().isEmpty())
+//                listaProjection.add(new ProjectionFilter("descricao", tipoReceituario.getDescricao()));
+//            else
+//                listaProjection.add(new ProjectionFilter("descricao"));
+//            
+//            if(tipoReceituario.getNumeroDiasValidadeReceita() != null && tipoReceituario.getNumeroDiasValidadeReceita() != 0L)
+//                listaProjection.add(new ProjectionFilter("numeroDiasValidadeReceita", tipoReceituario.getNumeroDiasValidadeReceita()));
+//            else
+//                listaProjection.add(new ProjectionFilter("numeroDiasValidadeReceita"));
+//            
+//            if(tipoReceituario.getQuantidadeViaReceita() != null)
+//                listaProjection.add(new ProjectionFilter("quantidadeViaReceita", tipoReceituario.getQuantidadeViaReceita()));
+//            else
+//                listaProjection.add(new ProjectionFilter("quantidadeViaReceita"));
+//
+//            if(tipoReceituario.getCor() != null && tipoReceituario.getCor() != -1)
+//                listaProjection.add(new ProjectionFilter("cor", tipoReceituario.getCor()));
+//            else
+//                listaProjection.add(new ProjectionFilter("cor"));
+//            
+//            listaProjection.add(new ProjectionFilter(Order.asc("descricao")));
+//            
+//            ProjectionFilter[] projectionFilters = new ProjectionFilter[listaProjection.size()];
+//
+//            listaProjection.toArray(projectionFilters);
+//            
+//            return projectionFilters;
+//        }
+    	
+    	return null;
     }
     
-    /*Metodo Responsável por criar os parametros de filtros para projeção
-     * 
-     * Ryan Fernandes
-     */
-
-    private ProjectionFilter[] criarProjection(TipoReceituario tipoReceituario)
+    public TipoReceituario salvar(TipoReceituario tipoReceituario)
     {
-
-        ArrayList<ProjectionFilter> listaProjection = new ArrayList<ProjectionFilter>();
-        
-        if(tipoReceituario.getId() != null)
-            listaProjection.add(new ProjectionFilter("id", tipoReceituario.getId()));
-        
-        listaProjection.add(new ProjectionFilter("id"));
-        if(tipoReceituario.getDescricao() != null && !tipoReceituario.getDescricao().isEmpty())
-            listaProjection.add(new ProjectionFilter("descricao", tipoReceituario.getDescricao()));
-        else
-            listaProjection.add(new ProjectionFilter("descricao"));
-        
-        if(tipoReceituario.getNumeroDiasValidadeReceita() != null && tipoReceituario.getNumeroDiasValidadeReceita() != 0L)
-            listaProjection.add(new ProjectionFilter("numeroDiasValidadeReceita", tipoReceituario.getNumeroDiasValidadeReceita()));
-        else
-            listaProjection.add(new ProjectionFilter("numeroDiasValidadeReceita"));
-        
-        if(tipoReceituario.getQuantidadeViaReceita() != null)
-            listaProjection.add(new ProjectionFilter("quantidadeViaReceita", tipoReceituario.getQuantidadeViaReceita()));
-        else
-            listaProjection.add(new ProjectionFilter("quantidadeViaReceita"));
-
-        if(tipoReceituario.getCor() != null && tipoReceituario.getCor() != -1)
-            listaProjection.add(new ProjectionFilter("cor", tipoReceituario.getCor()));
-        else
-            listaProjection.add(new ProjectionFilter("cor"));
-        
-        listaProjection.add(new ProjectionFilter(Order.asc("descricao")));
-        
-        ProjectionFilter[] projectionFilters = new ProjectionFilter[listaProjection.size()];
-
-        listaProjection.toArray(projectionFilters);
-        
-        return projectionFilters;
+        return tipoReceituarioRepository.save(tipoReceituario);
     }
 
-
-    @Override
-    public TipoReceituario salvar(TipoReceituario tipoReceituario) throws NotUniqueIdException, InstantiationException, IllegalAccessException,
-            ClassNotFoundException
+    public void excluir(TipoReceituario tipoReceituario)
     {
-        return save(tipoReceituario, true);
+    	tipoReceituarioRepository.delete(tipoReceituario);
     }
-
-    @Override
-    public void atualizar(TipoReceituario tipoReceituario) throws NotUniqueIdException, InstantiationException, IllegalAccessException,
-            ClassNotFoundException
-    {
-        update(tipoReceituario, true);
-    }
-
-    @Override
-    public void excluir(TipoReceituario tipoReceituario) throws NotUniqueIdException, InstantiationException, IllegalAccessException,
-            ClassNotFoundException
-    {
-        delete(tipoReceituario, true);
-    }
-
-    @Override
-    public Collection<TipoReceituario> list(TipoReceituario tipoReceituario)
-    {
-        if(tipoReceituario == null){
-            tipoReceituario = new TipoReceituario();
-        }
-        return get(0, criarProjection(tipoReceituario));
-    }
-    
-    @Override
-    public TipoReceituario getTipoReceituario(TipoReceituario tipoReceituario)
-    {
-        return getUnique(criarProjection(tipoReceituario));
-    }
-
     
 }
--- src/main/java/br/com/mv/dispensacaomedicamento/repository/TipoReceituarioRepository.java
+++ src/main/java/br/com/mv/dispensacaomedicamento/repository/TipoReceituarioRepository.java
@@ -1,10 +1,26 @@
 package br.com.mv.dispensacaomedicamento.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.mv.dispensacaomedicamento.model.TipoReceituario;
 
 public interface TipoReceituarioRepository extends CrudRepository<TipoReceituario,Long>
 {
-
+//	    public Collection<TipoReceituario> list(TipoReceituario tipoReceituario)
+//	    {
+//	    	return tipoReceituarioRepository.findAll(tipoReceituario);
+//	        if(tipoReceituario == null){
+//	            tipoReceituario = new TipoReceituario();
+//	        }
+//	        return get(0, criarProjection(tipoReceituario));
+//	    }
+	    public Collection<TipoReceituario> list(TipoReceituario tipoReceituario);
+	    
+//	    public TipoReceituario getTipoReceituario(TipoReceituario tipoReceituario)
+//	    {
+//	        return getUnique(criarProjection(tipoReceituario));
+//	    }
+	    public TipoReceituario getTipoReceituario(TipoReceituario tipoReceituario);
 }
